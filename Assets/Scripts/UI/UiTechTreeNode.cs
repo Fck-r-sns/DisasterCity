23a5783ec070069619340deeb28a4fa4d4393484
Fck-r-sns <fck.r.sns@gmail.com>
2018-03-25 12:20:10 +0300

    Tech tree WIP

---
+++ Assets/Scripts/UI/UiTechTreeNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class UiTechTreeNode : MonoBehaviour
+{
+    [SerializeField]
+    TechTreeNodeId _id;
+    [SerializeField]
+    UiTechTreeNode[] _parents;
+    [SerializeField]
+    UiTechTreeNode[] _children;
+
+    TechTreeNode _node;
+
+
+}
