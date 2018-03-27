6c0ed989fac1d45a1057e7c7293ae49a65f8ec1e
Fck-r-sns <fck.r.sns@gmail.com>
2018-03-27 23:21:15 +0300

    Units production WIP

---
+++ Assets/Scripts/UI/UiCallReserves.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UiCallReserves : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    Text _valueOutput;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Todo: call reserves
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Todo: show description
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Todo: hide description
+    }
+}
